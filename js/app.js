43c0e92a4d7e7549c88a71210d094487111615b1
Michael L Bangura <michlawbang123@gmail.com>
2022-09-01 06:56:26 -1200

    modify

--- js/app.js
+++ js/app.js
@@ -8,32 +8,32 @@ btn.addEventListener('click', () => {
 })
 
 
-// const fade_ins = document.querySelectorAll('.fade_in');
+const fade_ins = document.querySelectorAll('.fade_in');
 
-// // const sliders = document.querySelectorAll(".slide-in")
+// const sliders = document.querySelectorAll(".slide-in")
 
-// const optionsOnScroll = {
-//    threshold: 0.25,
-//    rootMargin: "0px 0px -100px 0px"
-// }
+const optionsOnScroll = {
+   threshold: 0.25,
+   rootMargin: "0px 0px -100px 0px"
+}
 
-// const animateOnScroll = new IntersectionObserver(function(entries, animateOnScroll) {
-//    // looping through the entries
-//    entries.forEach(entry => {
-//       if(!entry.isIntersecting) {
-//          return
-//       } else {
-//          // target the current entry and add the appear class to it
-//          entry.target.classList.add('appear');
-//          // unobserve all entry that is not intersecting
-//          animateOnScroll.unobserve(entry.target);
-//       }
-//    })
-// }, optionsOnScroll)
+const animateOnScroll = new IntersectionObserver(function(entries, animateOnScroll) {
+   // looping through the entries
+   entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+         return
+      } else {
+         // target the current entry and add the appear class to it
+         entry.target.classList.add('appear');
+         // unobserve all entry that is not intersecting
+         animateOnScroll.unobserve(entry.target);
+      }
+   })
+}, optionsOnScroll)
 
-// // Looping through the fadeIns and calling the animateOnScroll.observe() fnx
-// fade_ins.forEach(fade_in => {
-//    animateOnScroll.observe(fade_in)
-// });
+// Looping through the fadeIns and calling the animateOnScroll.observe() fnx
+fade_ins.forEach(fade_in => {
+   animateOnScroll.observe(fade_in)
+});
 
 
